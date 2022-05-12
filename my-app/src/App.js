ebb3cd41cb81c1e936071abc6dab0dc6139caecf
synerzip <synerzip@SYN-MacPro-85.local>
2022-05-12 16:03:52 +0530

    keys

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import CarList from './List/CarList';
+import PerfListWapper from './List/PerfList';
 
+const nodes = ['Hello', 'World']
 function App() {
   return (
     <div className="App">
       <CarList></CarList>
+      {/* <PerfListWapper nodes={nodes}></PerfListWapper> */}
     </div>
   );
 }
