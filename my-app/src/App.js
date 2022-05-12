10ba73228258073ccff82f2901efea083a3c6495
synerzip <synerzip@SYN-MacPro-85.local>
2022-05-12 10:55:56 +0530

    fixing imports

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
 import JSBindings from './binding/0_bindJS';
-import Bindings from './binding/5_classPropArrow';
 
-
-// import Bindings from './binding/1_noBind';
+import Bindings from './binding/1_noBind';
 // import Bindings from './binding/2_inlineBind';
-
-// import Bindings from './binding/4_constBind';
-// import Bindings from './binding/3_inlineArrow';
+//import Bindings from './binding/3_constBind';
+// import Bindings from './binding/4_inlineArrow';
+// import Bindings from './binding/5_classPropArrow';
 
 
 function App() {
