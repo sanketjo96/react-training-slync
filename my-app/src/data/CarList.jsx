15a99787b78d95b7d7a7745ee0c9e34284198aa4
synerzip <synerzip@SYN-MacPro-85.local>
2022-05-12 12:11:50 +0530

    added keys

--- my-app/src/data/CarList.jsx
+++ my-app/src/data/CarList.jsx
@@ -3,16 +3,19 @@ import './List.css';
 
 const cars = [
     {
+        id: '01111',
         name: 'Maruti800',
         company: 'Maruti',
         make: '1992',
     },
     {
+        id: '02222',
         name: 'H1678',
         company: 'Honda',
         make: '2008',
     },
     {
+        id: '03333',
         name: 'Indica',
         company: 'Tata',
         make: '2003',
@@ -27,7 +30,7 @@ export default class CarList extends Component {
                     {
                         cars.map(car => {
                             return (
-                                <li>{`${car.name} ${car.company} - ${car.make}`}</li>
+                                <li key={car.id}>{`${car.name} ${car.company} - ${car.make}`}</li>
                             )
                         })
                     }
