31e4e21f8a7999ee23d0eb14301eee7e4c9b7fe6
synerzip <synerzip@SYN-MacPro-85.local>
2022-05-14 15:24:48 +0530

    adding changes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,7 +5,7 @@ import MyClass from './Components/Class/MyClass';
 function App() {
   return (
     <div className="App">
-      <MyFunctional name={'john'}></MyFunctional>
+      {/* <MyFunctional name={'john'}></MyFunctional> */}
       <MyClass name={'john'}></MyClass>
     </div>
   );
