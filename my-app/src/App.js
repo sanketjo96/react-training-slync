9b3c07713dc2fd863023bbc26c29a89e60d654af
synerzip <synerzip@SYN-MacPro-85.local>
2022-05-25 17:16:49 +0530

    adding state + class compo

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,20 +1,29 @@
-import { useState } from 'react';
+import { Component, useState } from 'react';
 import './App.css';
 import Clock from './Clock/Clock';
 
-function App() {
-  const [isClock, setClock] = useState(true)
+class App extends Component {
+  constructor(props) {
+    super(props)
 
-  const destroryClock = () => {
-    setClock(false)
+    this.state = {
+      isClock: true,
+    }
   }
 
-  return (
-    <div className="App">
-      {isClock && <Clock/>}
-      <button onClick={destroryClock}>Destroy Clock</button>
-    </div>
-  );
+  destroryClock = () => {
+    this.setState({isClock: false})
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.isClock && <Clock />}
+        <button onClick={this.destroryClock}>Destroy Clock</button>
+      </div>
+    );
+  }
 }
+
 
 export default App;
