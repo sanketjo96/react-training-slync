b43f094915ae790f29e4bfed1376ef7220426d8f
synerzip <synerzip@SYN-MacPro-85.local>
2022-05-25 15:58:41 +0530

    minor tweak to getDerivedStateFromProps

--- my-app/src/Clock/Clock.jsx
+++ my-app/src/Clock/Clock.jsx
@@ -16,6 +16,7 @@ export default class Clock extends Component {
 
     static getDerivedStateFromProps() {
         console.log('Mounting: in getDerivedStateFromProps')
+        return null
     }
 
     componentDidMount() {
