325498cfab503ea24734308187911bd75c583fbe
Sanket <sanketjoshi96@gmail.com>
2022-05-31 15:02:32 +0530

    state intro

--- my-app/src/components/StateHook.jsx
+++ my-app/src/components/StateHook.jsx
@@ -1,15 +1,17 @@
 import React, { useState } from 'react'
 
+/**
+ * 1. Note the way to add state to functional component.
+ * 2. Setter is also available along with state data
+ * 3. Re-renders when setter get called
+ * 
+ */
 export default function StateHook() {
     const [count, setCount] = useState(0)
 
-    const increment = () => {
-        setCount(count + 1)
-    }
-
     return (
         <div>
-            <button onClick={() => increment()}>Increment {count}</button>
+            <button onClick={() =>  setCount(count + 1)}>Increment {count}</button>
         </div>
     )
 }
