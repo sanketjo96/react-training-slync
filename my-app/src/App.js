86a10162c761167416164a0d6fd136b64198c885
Sanket <sanketjoshi96@gmail.com>
2022-05-31 20:31:57 +0530

    cleanup

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,22 +1,22 @@
 
 import './App.css';
 // import ClassMouse from './components/ClassMouse';
-import FunctionMouse from './components/FunctionMouse';
+// import FunctionMouse from './components/FunctionMouse';
 // import ClassComponentCondition from './components/ClassComponentCondition';
 // import FunctionComponentCondition from './components/FunctionComponentCondition';
-// import ClassComponent from './components/ClassComponent';
+import ClassComponent from './components/ClassComponent';
 // import FunctionComponent from './components/FunctionComponent';
 
 function App() {
   return (
     <div className="App">
-      {/* <ClassComponent></ClassComponent>
-      <FunctionComponent></FunctionComponent> */}
+      <ClassComponent></ClassComponent>
+      {/* <FunctionComponent></FunctionComponent>  */}
       {/* <ClassComponentCondition></ClassComponentCondition> */}
       {/* <FunctionComponentCondition></FunctionComponentCondition> */}
 
       {/* <ClassMouse></ClassMouse> */}
-      <FunctionMouse></FunctionMouse>
+      {/* <FunctionMouse></FunctionMouse> */}
     </div>
   );
 }
