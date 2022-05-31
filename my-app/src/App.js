63e5311005abb934b767e337e33f9ac3e3d0ea92
Sanket <sanketjoshi96@gmail.com>
2022-05-31 12:52:10 +0530

    initia;

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,17 @@
 import { Component } from 'react';
 import './App.css';
-import MultiContext from './Components/MultiContext/MultiContext';
+// import MultiContext from './Components/MultiContext/MultiContext';
 // import WrapperWithContext from './Components/WithContext/WrapperWithContext';
-// import WrapperWithoutContext from './Components/WithoutContext/WrapperWithoutContext';
+import WrapperWithoutContext from './Components/WithoutContext/WrapperWithoutContext';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          {/* <WrapperWithoutContext></WrapperWithoutContext> */}
+          <WrapperWithoutContext></WrapperWithoutContext>
           {/* <WrapperWithContext></WrapperWithContext> */}
-          <MultiContext></MultiContext>
+          {/* <MultiContext></MultiContext> */}
         </header>
       </div>
     );
