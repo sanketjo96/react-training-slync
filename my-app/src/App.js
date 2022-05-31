55c94ecaa34613529a7b7488f6c01e0d17f6a69c
Sanket <sanketjoshi96@gmail.com>
2022-05-31 15:27:31 +0530

    clean

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import PostListFetch from './components/GET/PostListFetch';
-// import PostList from './components/GET/PostList';
+// import PostListFetch from './components/GET/PostListFetch';
+import PostList from './components/GET/PostList';
 
 function App() {
   return (
     <div className="App">
-      {/* <PostList></PostList> */}
-      <PostListFetch></PostListFetch>
+      <PostList></PostList>
+      {/* <PostListFetch></PostListFetch> */}
     </div>
   );
 }
