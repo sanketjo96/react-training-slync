0bfe3733033df01159b19ade0f5886e485b4557d
Sanket <sanketjoshi96@gmail.com>
2022-06-04 11:39:36 +0530

    remove original change

--- my-app/src/Components/MultiContext/Currency.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-export default class Currency extends Component {
-  render() {
-    return (
-      <div style={{color: this.props.theam}}>{this.props.currency}</div>
-    )
-  }
-}
