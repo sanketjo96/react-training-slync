445cba3afa23b60315102e5002dcd14938bb6cd5
Sanket <sanketjoshi96@gmail.com>
2022-06-06 15:43:23 +0530

    add cleanup

--- my-app/src/components/DataFetch.jsx
+++ my-app/src/components/DataFetch.jsx
@@ -6,23 +6,7 @@ import axios from 'axios';
  * Note the network tab and detect what should be done.
  */
 function DataFetch() {
-  const [posts, setPosts] = useState([])
 
-  useEffect(() => {
-    axios.get('https://jsonplaceholder.typicode.com/posts')
-    .then(res => {
-      setPosts(res.data)
-    })
-    .catch(e => console.log(e))
-  })
-
-  return (
-    <div>
-      {posts.map(post => (
-        <div>{post.title}</div>
-      ))}
-    </div>
-  )
 }
 
 export default DataFetch
