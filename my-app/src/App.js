de8d6c10e0ae5993eb99ae8c0e3985baf2e68faa
Sanket <sanketjoshi96@gmail.com>
2022-06-18 15:54:04 +0530

    init

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,9 @@
 import './App.css';
-// import ClassTimer from './Components/ClassTimer';
-import Timer from './Components/Timer';
-// import ClassRef from './Components/ClassRef';
-// import FunctionRef from './Components/FunctionRef';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <ClassRef></ClassRef>
-        <FunctionRef></FunctionRef> */}
-        {/* <ClassTimer></ClassTimer> */}
-        <Timer></Timer>
       </header>
     </div>
   );
